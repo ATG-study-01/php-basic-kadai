5d1ae7b53cdb8f66182cb84ad8414152445307d6
ATG-study-01 <88013709@atg.co.jp>
2026-03-10 16:40:15 +0900

    14章終了時課題

---
+++ kadai_sort/sort.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>PHP基礎編</title>
+</head>
+
+<body>
+ <p>
+  <?php
+  $num = [15, 4, 18, 23, 10];
+  sort($num);
+   echo '昇順にソートします。<br>';
+   foreach ($num as $val) {
+    echo "$val <br>";
+   }
+  rsort($num);
+   echo '降順にソートします。<br>';
+   foreach ($num as $val) {
+    echo "$val <br>";
+   }
+  ?>
+ </p> 
+</body>
+</html>
